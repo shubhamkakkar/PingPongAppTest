5a4b17a4603409042bd0891a10d790663478d453
shubhamkakkar <shubham.kakkar98@gmail.com>
2020-07-02 19:23:43 +0530

    lottie on work

--- src/Screens/SplashScreen/SplashScreen.tsx
+++ src/Screens/SplashScreen/SplashScreen.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { View } from 'react-native';
+import LottieView from 'lottie-react-native';
+import styles from './splashScreenStyleSheet';
+import pingPongLottie from '../../assets/pingPongLottie.json';
 
 export default function SplashScreen() {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.lottieContainer}>
+        <LottieView source={pingPongLottie} autoPlay loop />
+      </View>
+      <View style={styles.appNameContainer} />
+    </View>
+  );
 }
